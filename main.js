04eae4d27c4e6e0b050a3fa7de0d60ca26f2ea81
Egoist <lisovskiy00@gmail.com>
2021-02-11 00:41:19 +0300

    Main module set up
    Setting up Main module with harvesters only

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+//init roles
+var roleHarvester = require('role.harvester');
+
+
 module.exports.loop = function () {
 
+  //creep amount check
+  var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+  console.log('Harvesters: ' + harvesters.length);
+
+  //autospawn creeps with roles
+  if(harvesters.length < 2) {
+      var newName = 'Harvester' + Game.time;
+      console.log('Spawning new harvester: ' + newName);
+      Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
+          {memory: {role: 'harvester'}});
+  }
+
+
+  //init behavior based on role
+  for(var name in Game.creeps){
+    var creep = Game.creeps[name];
+    
+    if (creep.memory.role == 'harvester')
+      roleHarvester.run(creep);
+
+    }
+    
 }
